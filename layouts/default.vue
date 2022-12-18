efa3882dc0e108eac0ac1ee13e49b4934b57f9fe
ahmedElnewery <ahmedm.elnewery@gmail.com>
2022-12-18 05:16:27 +0200

    init colors and font size layout

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+
+
+<script>
+export default {
+
+}
+</script>
+<template>
+  <div>
+    <nav class="flex items-center justify-between container mx-auto py-4 px-12 h-24">
+        <div>Ahmed</div>
+        <ul class="flex items-center">
+            <li>About</li>
+            <li>Experience</li>
+            <li>Work</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+    <div class="container">
+        <slot/>
+    </div>
+  </div>
+</template>
+<style>
+
+</style>
