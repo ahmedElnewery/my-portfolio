bac1b4ead4d1a2d0c77b2799c8f337d8d56c9999
ahmedElnewery <ahmedm.elnewery@gmail.com>
2023-01-02 00:43:10 +0200

    change layout

--- constants/constants.js
+++ constants/constants.js
@@ -1,8 +1,14 @@
+import Message from './../components/icons/Message.vue'
+import BriefCase from './../components/icons/BriefCase.vue'
+import Code from './../components/icons/Code.vue'
+import External from './../components/icons/External.vue'
+import Home from './../components/icons/Home.vue'
 export const navbar = [
-    { label: "About", path: "/#about" },
-    { label: "Experience", path: "/#experience" },
-    { label: "projects", path: "/#projects" },
-    { label: "Contact", path: "/#contact" },
+    { label: "Home", path: "/",icon:Home },
+    { label: "About", path: "/about",icon:Message },
+    { label: "Experience", path: "/experience",icon:BriefCase },
+    { label: "projects", path: "/projects",icon:Code },
+    { label: "Contact", path: "/contact",icon:External },
 ]
 export const experiences = [
     {
@@ -74,3 +80,113 @@ export const skills = [
     "Server Side Rendering (Next , Nuxt)",
     "Unit Testing (Jest)"
 ]
+export const options = {
+    particles: {
+      number: {
+        value: 160,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+        polygon: {
+          nb_sides: 5,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
+      },
+      opacity: {
+        value: 1,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0,
+          sync: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 4,
+          size_min: 0.3,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: false,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 600,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "bubble",
+        },
+        onclick: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
+        },
+        bubble: {
+          distance: 250,
+          size: 0,
+          duration: 2,
+          opacity: 0,
+          speed: 3,
+        },
+        repulse: {
+          distance: 400,
+          duration: 0.4,
+        },
+        push: {
+          particles_nb: 4,
+        },
+        remove: {
+          particles_nb: 2,
+        },
+      },
+    },
+    retina_detect: true,
+  };
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,23 @@
 
 
 <script setup>
-import { navbar } from "./../constants/constants";
+import { navbar, options } from "./../constants/constants";
+import { loadFull } from "tsparticles";
 
+// particles
+const particlesInit = async (engine) => {
+  await loadFull(engine);
+};
 </script>
 <template>
   <div class="min-h-screen">
-    
+    <Particles
+      id="tsparticles"
+      :particlesInit="particlesInit"
+      :options="options"
+    />
     <div class="relative z-10">
-      <header class="w-full sticky top-0">
+      <!-- <header class="w-full sticky top-0">
         <nav class="flex items-center justify-between py-4 px-12 h-24">
           <NuxtLink to="/" class="text-green w-10 h-10 flex-shrink-0"
             ><Logo />
@@ -24,8 +33,8 @@ import { navbar } from "./../constants/constants";
             <button class="btn-primary btn-sm ml-4">Resume</button>
           </div>
         </nav>
-      </header>
-      <div class="w-10 fixed left-10 bottom-0 md:block hidden">
+      </header> -->
+      <div class="w-10 fixed right-10 bottom-0 md:block hidden">
         <ul
           class="
             flex flex-col
@@ -97,32 +106,12 @@ import { navbar } from "./../constants/constants";
           </li>
         </ul>
       </div>
-      <div class="w-10 fixed right-10 bottom-0 md:block hidden">
-        <ul
-          class="
-            flex flex-col
-            items-center
-            text-sm
-            after:w-px after:h-32 after:bg-light-slate after:block
-          "
-        >
-          <li class="">
-            <a
-              class="
-                p-2.5
-                block
-                cursor-pointer
-                hover:text-green
-                transform
-                hover:-translate-y-0.5
-                transition-all
-                vertical-mode
-                mb-6
-              "
-              href="mailto:ahmedm.elnewery@gmail.com"
-            >
-              ahmedm.elnewery@gmail.com</a
-            >
+      <div class="w-10 fixed left-10 top-1/2 -translate-y-1/2 md:block hidden">
+        <ul class="flex flex-col items-center text-sm font-mono border rounded-full min-w-min px-2 py-8">
+          <li class="" v-for="(item, i) in navbar" :key="i">
+            <NuxtLink :to="item.path" class="navbar-item mb-2">
+              <component :is="item.icon" />
+            </NuxtLink>
           </li>
         </ul>
       </div>
@@ -132,9 +121,7 @@ import { navbar } from "./../constants/constants";
           min-h-screen
           py-0
           px-12
-          md:px-28
-          xl:px-32
-          2xl:px-48
+          md:px-48
           w-full
           h-full
           p-t-[100px]
