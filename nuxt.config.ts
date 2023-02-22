5090dec1468333dda97fd551d364eeb213e9a341
ahmedElnewery <ahmedm.elnewery@gmail.com>
2023-02-22 20:09:21 +0200

    adding meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-   app:{
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  app: {
     // pageTransition: { name: 'page', mode: 'out-in' },
-    head:{
-      title:"Ahmed El Newery",
-      link:[ { rel:"icon", href: 'https://raw.github.com/eladnava/material-letter-icons/master/dist/png/A.png' }]
-    }
-   },
-   plugins:[
-    { src: '~/plugins/particles', ssr: false },
-    { src: '~/plugins/store', ssr: false }
+    head: {
+      title: "Ahmed El Newery",
+      link: [
+        {
+          rel: "icon",
+          href: "https://raw.github.com/eladnava/material-letter-icons/master/dist/png/A.png",
+        },
+      ],
+      meta: [
+        {
+          name: "description",
+          content:
+            "Ahmed El Newery, Frontend engineer, I am Goal-oriented Full stack developer experienced in front-end , skilled at building optimized and reusable user interface components and design an efficient layout. Optimized solution seeker and challenge lover who loving learning and gaining new experience. ",
+        },
 
-   ]   
-})
+        { property: "og:type", content: "website" },
+        { property: "og:url", content: "https://elnewery.netlify.app/" },
+        { property: "og:title", content: "Ahmed El Newery" },
+        { property: "og:description", content: "website" },
+        // { property: "og:image", content: "" },
+        { property: "twitter:card", content: "summary_large_image" },
+        { property: "twitter:url", content: "https://elnewery.netlify.app/" },
+        { property: "twitter:title", content: "Ahmed El Newery" },
+        { property: "twitter:description", content: 
+        "Ahmed El Newery, Frontend engineer, I am Goal-oriented Full stack developer experienced in front-end , skilled at building optimized and reusable user interface components and design an efficient layout. Optimized solution seeker and challenge lover who loving learning and gaining new experience. ",
+
+      },
+        // { property: "twitter:image", content: "" },
+      ],
+    },
+  },
+  plugins: [
+    { src: "~/plugins/particles", ssr: false },
+    { src: "~/plugins/store", ssr: false },
+  ],
+});
